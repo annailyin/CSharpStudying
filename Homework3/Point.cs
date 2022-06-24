20172cc1243899e7dc1a5a12049963c13b06bc77
Anna Ilyinkova <anna.ilyin@gmail.com>
2022-06-24 14:29:58 +0300

    Homework #3

---
+++ Homework3/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpStudyingApp
+{
+    public class Point
+    {
+        private readonly decimal _x, _y;
+
+        public decimal X
+        {
+            get { return _x; }
+        }
+
+        public decimal Y
+        {
+            get { return _y; }
+        }
+
+        public Point (decimal x, decimal y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public override string ToString()
+        {
+            return $"x = {_x}, y = {_y}";
+        }
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpStudyingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[5];
+            for (int i = 0; i < 5; i++)
+            {
+                points[i] = new Point(i + 0.555m, i + 0.777m);
+                Console.WriteLine(points[i]);
+            }
+            //Adding points
+            Point point = points[0] + points[1];
+            Console.WriteLine("The result of adding the 1st and the 2nd points: ");
+            Console.WriteLine(point);
+        }
+    }
+}
